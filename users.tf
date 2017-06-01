efd5e475815904d50c6d5a381a4b110ed1f93848
Matt Jones <urlugal@gmail.com>
2017-06-01 10:47:59 -0400

    testing

--- users.tf
+++ users.tf
@@ -12,13 +12,20 @@ variable "users" {
       name  = "User Two"
       email = "user.two@gmail.com"
       teams = ["Team Two"]
-      role  = "limited_user"
+      role  = "admin"
     },
-    {
-      name  = "User Three"
-      email = "user.three@gmail.com"
-      teams = ["Team One", "Team Two"]
-      role  = "limited_user"
-    },
+    # {
+    #   name  = "User Four"
+    #   email = "user.four@gmail.com"
+    #   teams = ["Team Two"]
+    #   role  = "limited_user"
+    # },
   ]
+
+  # {
+  #   name  = "User Three"
+  #   email = "user.three@gmail.com"
+  #   teams = ["Team One", "Team Two"]
+  #   role  = "limited_user"
+  # },
 }
