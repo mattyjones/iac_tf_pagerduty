1b71da1477d6129c5e82c6983fd808e1dbfe0d17
Matt Jones <urlugal@gmail.com>
2017-06-01 20:35:02 -0400

    make users dependent upon teams

--- users.tf
+++ users.tf
@@ -1,24 +1,22 @@
-variable "users" {
-  type = "list"
-
-  default = [
-    {
-      name  = "User One"
-      email = "user.one@gmail.com"
-      teams = ["Team One"]
-      role  = "limited_user"
-    },
-    {
-      name  = "User Two"
-      email = "user.two@gmail.com"
-      teams = ["Team Two", "Team Three"]
-      role  = "limited_user"
-    },
-    {
-      name  = "User Three"
-      email = "user.three@gmail.com"
-      teams = ["Team One", "Team Two", "Team Three"]
-      role  = "limited_user"
-    },
-  ]
+resource "pagerduty_user" "User_One" {
+  name  = "User One"
+  email = "user.one@gmail.com"
+  role  = "limited_user"
+  teams = ["${pagerduty_team.Team_Two.id}"]
 }
+
+resource "pagerduty_user" "User_Two" {
+  name  = "User Two"
+  email = "user.two@gmail.com"
+  role  = "limited_user"
+  teams = ["${pagerduty_team.Team_Two.id}","${pagerduty_team.Team_Three.id}"]
+}
+
+resource "pagerduty_user" "User_Three" {
+  name  = "User Three"
+  email = "user.three@gmail.com"
+  role  = "limited_user"
+  teams = ["${pagerduty_team.Team_Two.id}", "${pagerduty_team.Team_One.id}"]
+}
+
+
