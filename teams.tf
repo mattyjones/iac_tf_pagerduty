7c2da2c877d7128ee2eb9573ec5c26b22e326cdd
Matt Jones <urlugal@gmail.com>
2017-06-02 10:44:01 -0400

    general cleanup

--- teams.tf
+++ teams.tf
@@ -1,27 +1,27 @@
-resource "pagerduty_team" "Team_One" {
-  name        = "Team One"
+# Pagerduty teams
+resource "pagerduty_team" "Team_Five" {
+  name        = "Team Five"
 }
-
-resource "pagerduty_team" "Team_Two" {
-  name        = "Team Two"
-}
-
-resource "pagerduty_team" "Team_Three" {
-  name        = "Team Three"
-}
-
 resource "pagerduty_team" "Team_Four" {
   name        = "Team Four"
 }
 
-resource "pagerduty_team" "Team_Five" {
-  name        = "Team Five"
+resource "pagerduty_team" "Team_One" {
+  name        = "Team One"
+}
+
+resource "pagerduty_team" "Team_Seven" {
+  name        = "Team Eight"
 }
 
 resource "pagerduty_team" "Team_Six" {
   name        = "Team Six"
 }
 
-resource "pagerduty_team" "Team_Seven" {
-  name        = "Team Eight"
+resource "pagerduty_team" "Team_Three" {
+  name        = "Team Three"
+}
+
+resource "pagerduty_team" "Team_Two" {
+  name        = "Team Two"
 }
--- users.tf
+++ users.tf
@@ -1,8 +1,16 @@
-resource "pagerduty_user" "User_One" {
-  name  = "User One"
-  email = "user.one@gmail.com"
+# Pagerduty Users
+resource "pagerduty_user" "User_Four" {
+  name  = "User Four"
+  email = "user.four@gmail.com"
   role  = "limited_user"
-  teams = ["${pagerduty_team.Team_Two.id}"]
+  teams = ["${pagerduty_team.Team_Two.id}", "${pagerduty_team.Team_One.id}"]
+}
+
+resource "pagerduty_user" "User_Three" {
+  name  = "User Three"
+  email = "user.three@gmail.com"
+  role  = "limited_user"
+  teams = ["${pagerduty_team.Team_One.id}","${pagerduty_team.Team_Three.id}"]
 }
 
 resource "pagerduty_user" "User_Two" {
@@ -11,12 +19,9 @@ resource "pagerduty_user" "User_Two" {
   role  = "limited_user"
   teams = ["${pagerduty_team.Team_Two.id}","${pagerduty_team.Team_Three.id}"]
 }
-
-resource "pagerduty_user" "User_Three" {
-  name  = "User Three"
-  email = "user.three@gmail.com"
+resource "pagerduty_user" "User_One" {
+  name  = "User One"
+  email = "user.one@gmail.com"
   role  = "limited_user"
-  teams = ["${pagerduty_team.Team_Two.id}", "${pagerduty_team.Team_One.id}"]
+  teams = ["${pagerduty_team.Team_Two.id}"]
 }
-
-
